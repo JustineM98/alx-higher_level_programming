4db56ce625ccbec12dc63aa51f5b587ba0cadd16
JustineM98 <justinemugishe98@gmail.com>
2022-06-24 17:49:06 +0200

    My 0x06-python-classes project

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -51,4 +51,4 @@ class Square:
 
     def __ge__(self, other):
         """Define the >= compmarison to a Square."""
-        return self.area() >= other.a
+        return self.area() >= other.area()
