ac923b382fa1ff0206ce4b347848c44419823c2f
JustineM98 <justinemugishe98@gmail.com>
2022-06-25 22:12:33 +0200

    test

--- 0x07-python-test_driven_development/tests/5-main.py
+++ 0x07-python-test_driven_development/tests/5-main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 text_indentation = __import__('5-text_indentation').text_indentation
 
-text_indentation(
+text_indentation(""" check that a module’s docstrings are up-to-date by verifying that all interactive examples still work as documented.
+To perform regression testing by verifying that interactive examples from a test file or a test object work as expected.
+To write tutorial documentation for a package, liberally illustrated with input-output examples. Depending on whether the examples or the expository text are emphasized, this has the flavor of “literate testing” or “executable documentation”.""")
